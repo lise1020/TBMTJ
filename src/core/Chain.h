662345ec97b3ff8eb276a0a079e89148bb47b970
LISE <lise811020@gmail.com>
2015-10-28 10:53:09 +0800

    update

--- src/core/Chain.h
+++ src/core/Chain.h
@@ -15,15 +15,23 @@ public:
 
     Chain(const Lead& lL, const arma::mat& T1, const Insulator& B, const arma::mat& T2, const Lead& lR);
     Chain(const Lead& lL, const std::string& T1, const Insulator& B, const std::string& T2, const Lead& lR);
-    mat H(double o1, double o2);
-    cx_mat G_R(double o1, double o2, double E);
-    //cx_mat G_A(double o1, double o2, double E);
-    cx_mat G_L(double o1, double o2, double E);
+       mat H(const double o1, const double o2);
+    cx_mat G_R(const double o1, const double o2, const double E);
+  //cx_mat G_A(const double o1, const double o2, const double E);
+    cx_mat G_L(const double o1, const double o2, const double E);
 
+	//TODO
     double unname_x(double o1, double o2, double E);
     double unname_y(double o1, double o2, double E);
     double unname_z(double o1, double o2, double E);
     std::vector<double> unname2(double o1, double o2, double E);
+
+/*private:
+
+	   mat   _H;
+	cx_mat _G_R;
+	cx_mat _G_L;*/
+
 };
 
 
